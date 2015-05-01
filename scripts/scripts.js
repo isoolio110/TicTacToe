a130cd8cb75a5b052f77f68103f7cead31ebae02
isoolio110 <kim.isul@gmail.com>
2015-05-01 14:32:19 -0400

    Adds nextMove function that updates the tictactoe squares with the players move

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,11 +2,15 @@ var turn
 
 function startGame() {
   turn = "X";
-  setMessage(turn + " gets to start.")
+  setMessage(turn + " gets to start.");
 }
 
 function setMessage(msg){
   document.getElementById("message").innerText = msg;
+}
+
+function nextMove(square) {
+  square.innerText = document.turn;
 }
 
 $(function(){
