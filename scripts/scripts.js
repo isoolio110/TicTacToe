6fce08ead50309ad793b3cd2ea9d7341a5a81665
isoolio110 <kim.isul@gmail.com>
2015-05-01 14:19:58 -0400

    Adds ability to set message to user in startGame function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,8 +2,9 @@ var turn
 
 function startGame() {
   turn = "X";
+  setMessage(turn + " gets to start.")
 }
 
 function setMessage(msg){
-  document.getElementById("message")
+  document.getElementById("message").innerText = msg;
 }
